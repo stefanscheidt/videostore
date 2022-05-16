703db5e70a7d45d80dbdf2b29aeb310ba8ea289d
Stefan Scheidt <stefan.scheidt@rewe-digital.com>
2022-05-16 15:33:17 +0200

    Update dependencies and use Java 17
    Also use text blocks in tests.

--- src/test/java/imaginary/videostore/CustomerTest.java
+++ src/test/java/imaginary/videostore/CustomerTest.java
@@ -24,39 +24,43 @@ public class CustomerTest {
 
     @Test
     public void statement_NoRentals() {
-        String expectedStatement = "Rental Record for Customer\n"
-                + "Amount owed is 0.0\n"
-                + "You earned 0 frequent renter points";
-        assertThat(customer.statement()).as("should generate 'empty' statment.").isEqualTo(expectedStatement);
+        String expectedStatement = """
+                Rental Record for Customer
+                Amount owed is 0.0
+                You earned 0 frequent renter points""";
+        assertThat(customer.statement()).isEqualTo(expectedStatement);
     }
 
     @Test
     public void statement_OneRegularMovieTwoDays() {
         customer.addRental(new Rental(regularMovie1, 2));
-        String expectedStatement = "Rental Record for Customer\n"
-                + "\tRegular 1\t2.0\n"
-                + "Amount owed is 2.0\n"
-                + "You earned 1 frequent renter points";
+        String expectedStatement = """
+                Rental Record for Customer
+                \tRegular 1\t2.0
+                Amount owed is 2.0
+                You earned 1 frequent renter points""";
         assertThat(customer.statement()).isEqualTo(expectedStatement);
     }
 
     @Test
     public void statement_OneRegularMovieThreeDays() {
         customer.addRental(new Rental(regularMovie1, 3));
-        String expectedStatement = "Rental Record for Customer\n"
-                + "\tRegular 1\t3.5\n"
-                + "Amount owed is 3.5\n"
-                + "You earned 1 frequent renter points";
+        String expectedStatement = """
+                Rental Record for Customer
+                \tRegular 1\t3.5
+                Amount owed is 3.5
+                You earned 1 frequent renter points""";
         assertThat(customer.statement()).isEqualTo(expectedStatement);
     }
 
     @Test
     public void statement_OneRegularMovieFourDays() {
         customer.addRental(new Rental(regularMovie1, 4));
-        String expectedStatement = "Rental Record for Customer\n"
-                + "\tRegular 1\t5.0\n"
-                + "Amount owed is 5.0\n"
-                + "You earned 1 frequent renter points";
+        String expectedStatement = """
+                Rental Record for Customer
+                \tRegular 1\t5.0
+                Amount owed is 5.0
+                You earned 1 frequent renter points""";
         assertThat(customer.statement()).isEqualTo(expectedStatement);
     }
 
@@ -64,61 +68,67 @@ public class CustomerTest {
     public void statement_TwoRegularMovies() {
         customer.addRental(new Rental(regularMovie1, 2));
         customer.addRental(new Rental(regularMovie2, 3));
-        String expectedStatement = "Rental Record for Customer\n"
-                + "\tRegular 1\t2.0\n"
-                + "\tRegular 2\t3.5\n"
-                + "Amount owed is 5.5\n"
-                + "You earned 2 frequent renter points";
+        String expectedStatement = """
+                Rental Record for Customer
+                \tRegular 1\t2.0
+                \tRegular 2\t3.5
+                Amount owed is 5.5
+                You earned 2 frequent renter points""";
         assertThat(customer.statement()).isEqualTo(expectedStatement);
     }
 
     @Test
     public void statement_OneNewReleaseOneDay() {
         customer.addRental(new Rental(newReleaseMovie, 1));
-        String expectedStatement = "Rental Record for Customer\n"
-                + "\tNew Release\t3.0\n"
-                + "Amount owed is 3.0\n"
-                + "You earned 1 frequent renter points";
+        String expectedStatement = """
+                Rental Record for Customer
+                \tNew Release\t3.0
+                Amount owed is 3.0
+                You earned 1 frequent renter points""";
         assertThat(customer.statement()).isEqualTo(expectedStatement);
     }
 
     @Test
     public void statement_OneNewReleaseTwoDays() {
         customer.addRental(new Rental(newReleaseMovie, 2));
-        String expectedStatement = "Rental Record for Customer\n"
-                + "\tNew Release\t6.0\n"
-                + "Amount owed is 6.0\n"
-                + "You earned 2 frequent renter points";
+        String expectedStatement = """
+                Rental Record for Customer
+                \tNew Release\t6.0
+                Amount owed is 6.0
+                You earned 2 frequent renter points""";
         assertThat(customer.statement()).isEqualTo(expectedStatement);
     }
 
     @Test
     public void statement_OneChildrenThreeDays() {
         customer.addRental(new Rental(childrenMovie, 3));
-        String expectedStatement = "Rental Record for Customer\n"
-                + "\tChildren\t1.5\n"
-                + "Amount owed is 1.5\n"
-                + "You earned 1 frequent renter points";
+        String expectedStatement = """
+                Rental Record for Customer
+                \tChildren\t1.5
+                Amount owed is 1.5
+                You earned 1 frequent renter points""";
         assertThat(customer.statement()).isEqualTo(expectedStatement);
     }
 
     @Test
     public void statement_OneChildrenFourDays() {
         customer.addRental(new Rental(childrenMovie, 4));
-        String expectedStatement = "Rental Record for Customer\n"
-                + "\tChildren\t3.0\n"
-                + "Amount owed is 3.0\n"
-                + "You earned 1 frequent renter points";
+        String expectedStatement = """
+                Rental Record for Customer
+                \tChildren\t3.0
+                Amount owed is 3.0
+                You earned 1 frequent renter points""";
         assertThat(customer.statement()).isEqualTo(expectedStatement);
     }
 
     @Test
     public void statement_OneChildrenFiveDays() {
         customer.addRental(new Rental(childrenMovie, 5));
-        String expectedStatement = "Rental Record for Customer\n"
-                + "\tChildren\t4.5\n"
-                + "Amount owed is 4.5\n"
-                + "You earned 1 frequent renter points";
+        String expectedStatement = """
+                Rental Record for Customer
+                \tChildren\t4.5
+                Amount owed is 4.5
+                You earned 1 frequent renter points""";
         assertThat(customer.statement()).isEqualTo(expectedStatement);
     }
 
@@ -128,13 +138,14 @@ public class CustomerTest {
         customer.addRental(new Rental(regularMovie2, 3));
         customer.addRental(new Rental(newReleaseMovie, 3));
         customer.addRental(new Rental(childrenMovie, 5));
-        String expectedStatement = "Rental Record for Customer\n"
-                + "\tRegular 1\t2.0\n"
-                + "\tRegular 2\t3.5\n"
-                + "\tNew Release\t9.0\n"
-                + "\tChildren\t4.5\n"
-                + "Amount owed is 19.0\n"
-                + "You earned 5 frequent renter points";
+        String expectedStatement = """
+                Rental Record for Customer
+                \tRegular 1\t2.0
+                \tRegular 2\t3.5
+                \tNew Release\t9.0
+                \tChildren\t4.5
+                Amount owed is 19.0
+                You earned 5 frequent renter points""";
         assertThat(customer.statement()).isEqualTo(expectedStatement);
     }
 
